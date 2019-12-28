90af3a65e51cb52dd987fa6457f7f679c9a31d19
ishita159 <singhishita159@gmail.com>
2019-12-28 13:45:07 +0530

    basic codes

---
+++ decimal_to_others.py
@@ -0,0 +1,5 @@
+dec = 344
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
---
+++ no_divisible_by_another.py
@@ -0,0 +1,5 @@
+my_list = [12, 65, 54, 39, 102, 339, 221,]
+# use anonymous function to filter
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+
+print("Numbers divisible by 13 are",result)
